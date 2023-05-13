2b9473485e00db54ee3c7fa69ca83b73557da635
Satya <satya9731@gmail.com>
2023-05-13 19:30:00 +0530

    Add icons and images

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,33 @@
+import Add from './Add.svg';
+import Bookmark from './Bookmark.svg';
+import Comments from './Comments.svg';
+import Correct from './Correct.svg';
+import Discover from './Discover.svg';
+import Flip from './Flip.svg';
+import Home from './Home.svg';
+import Like from './Like.svg';
+import Playlist from './Playlist.svg';
+import Profile from './Profile.svg';
+import Right from './Right.svg';
+import Search from './Search.svg';
+import Share from './Share.svg';
+import Stopwatch from './Stopwatch.svg';
+import Wrong from './Wrong.svg';
+
+export {
+  Add,
+  Bookmark,
+  Comments,
+  Correct,
+  Discover,
+  Flip,
+  Home,
+  Like,
+  Playlist,
+  Profile,
+  Right,
+  Search,
+  Share,
+  Stopwatch,
+  Wrong,
+};
